112323f058565b0e116e71b0ecdf4e95e13c4f87
priyanshu jain <68162762+priyanshu003@users.noreply.github.com>
2022-06-20 00:27:25 +0530

    Time: 5 ms (59.65%), Space: 42.1 MB (58.54%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,36 +1,36 @@
 class Solution {
-       public String reorganizeString(String S) {
-        int[] hash = new int[26];
-        for (int i = 0; i < S.length(); i++) {
-            hash[S.charAt(i) - 'a']++;
-        } 
-        int max = 0, letter = 0;
-        for (int i = 0; i < hash.length; i++) {
-            if (hash[i] > max) {
-                max = hash[i];
-                letter = i;
-            }
+    public String reorganizeString(String S) {
+      
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : S.toCharArray()) {
+            int count = map.getOrDefault(c, 0) + 1;
+          
+            if (count > (S.length() + 1) / 2) return "";
+            map.put(c, count);
         }
-        if (max > (S.length() + 1) / 2) {
-            return ""; 
+      
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (char c : map.keySet()) {
+            pq.add(new int[] {c, map.get(c)});
         }
-        char[] res = new char[S.length()];
-        int idx = 0;
-        while (hash[letter] > 0) {
-            res[idx] = (char) (letter + 'a');
-            idx += 2;
-            hash[letter]--;
-        }
-        for (int i = 0; i < hash.length; i++) {
-            while (hash[i] > 0) {
-                if (idx >= res.length) {
-                    idx = 1;
+       
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            int[] first = pq.poll();
+            if (sb.length() == 0 || first[0] != sb.charAt(sb.length() - 1)) {
+                sb.append((char) first[0]);
+                if (--first[1] > 0) {
+                    pq.add(first);
                 }
-                res[idx] = (char) (i + 'a');
-                idx += 2;
-                hash[i]--;
+            } else {
+                int[] second = pq.poll();
+                sb.append((char) second[0]);
+                if (--second[1] > 0) {
+                    pq.add(second);
+                }
+                pq.add(first);
             }
         }
-        return String.valueOf(res);
+        return sb.toString();
     }
 }
